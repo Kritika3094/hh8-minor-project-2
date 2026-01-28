b2f4190624f917914b69b573fc76ad8e518dcd2f
Kritika3094 <kritikaaa0730@gmail.com>
2026-01-28 20:01:31 +0530

    Added Express server setup for XSS lab

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+
 const app = express();
 const PORT = 3000;
 
+
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 let comments = []; // Stored comments (vulnerable)
 
@@ -20,4 +23,23 @@ app.post("/comment", (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
+});
+app.get("/search", (req, res) => {
+  res.render("reflected", { query: req.query.q });
+});
+app.get("/dom", (req, res) => {
+  res.render("dom");
+});
+app.get("/set-cookie", (req, res) => {
+  res.cookie("sessionID", "KRITIKA123");
+  res.send("Cookie set!");
+});
+
+app.get("/secure", (req, res) => {
+  res.render("secure", { comments });
+});
+
+app.post("/secure-comment", (req, res) => {
+  comments.push(req.body.comment);
+  res.redirect("/secure");
 });
